b52293e16dcc134422897971207ceb3783fbb032
Pavan Hassamal <phass001@gmail.com>
2018-08-21 21:22:08 -0700

    annoyMatic

---
+++ annoyMatic.js
@@ -0,0 +1,14 @@
+// var answer = prompt("are we there yet?");
+
+// while(answer !== "yes" && answer !== "yeah" || answer = "yes + "" ") {
+// 	var answer = prompt("are we there yet");
+// }
+// alert("Yay, we made it");
+
+
+var answer = prompt("are we there yet?");
+
+while(answer.indexOf("yes") === -1) {
+	var answer = prompt("are we there yet");
+}
+alert("Yay, we made it");
